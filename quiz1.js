4575c7647eff721b9bfc71eb5fbf0f31842677a2
Ruby Ou <rubyruru3205@gmail.com>
2021-04-05 11:00:44 +0800

    quiz#2

--- quiz1.js
+++ quiz1.js
@@ -1,7 +1,6 @@
-function change() {     
-    var header = document.getElementById("H1");     
-    header.innerHTML = "CSIE@CGU";     
-    var para = document.getElementById("P");     
+function change() {        
+    $('#H1').innerHTML = "CSIE@CGU";     
+    var $para = $('#P');     
     para.innerHTML = "怎麼那麼棒！！."; 
 }   
 
