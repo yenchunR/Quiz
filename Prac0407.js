09c8765152f78a818d542301820fadadf5d0a3be
Ruby Ou <rubyruru3205@gmail.com>
2021-04-07 17:04:28 +0800

    quiz#2第二次

--- Prac0407.js
+++ Prac0407.js
@@ -1,12 +1,13 @@
 var tags = "貓"; 
 var dataUrl = "https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags +"&tagmode=any&format=json&per_page=400&jsoncallback=?";
-var data = $.getJSON(dataUrl); data.done( function( msg ) 
+var data = $.getJSON(dataUrl); 
+data.done( function( msg ) 
 { 
-   $.each(msg.items, function(i, item){
-    console.log(msg);
-   $("#contain").html();
-   $("#contain").append($("<img/>").attr("src", item.media.m));
-   })
+    $.each(msg.items, function(i, item){
+        console.log(msg);
+       $("#contain").html();
+       $("#contain").append($("<h2>" + item.title +"</h2>"));
+       })
 });
 data.fail( function( msg ) {
   console.log(msg);
